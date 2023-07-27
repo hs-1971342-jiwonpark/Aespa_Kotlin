c70b335a3b6218f811c289bcb83b58c25606f640
jiwon <pjw99032986@gmail.com>
2023-07-27 19:11:44 +0900

    Initial commit

---
+++ app/src/main/java/com/example/aespa/SummaryPage.kt
@@ -0,0 +1,11 @@
+package com.example.aespa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SummaryPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_summary_page)
+    }
+}
